f947c0d98811dbbff611dd7d68b6d6a7f24edaa4
Nghi Tran <pnghi320@gmail.com>
2021-05-29 20:52:19 -0700

    Update WeightManager.cs

--- Assets/Scripts/WeightManager.cs
+++ Assets/Scripts/WeightManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WeightManager : MonoBehaviour
 {
     public AssembleModel snapObjects;
+    private bool oneWeightIn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +15,19 @@ public class WeightManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (snapObjects)
+        if (snapObjects && !oneWeightIn)
         {
             if (gameObject.GetComponent<AssembleModel>().snapped[0] || gameObject.GetComponent<AssembleModel>().snapped[0])
             {
                 gameObject.GetComponent<AssembleModel>().snapped[2] = true;
                 gameObject.GetComponent<AssembleModel>().snapped[3] = true;
+                oneWeightIn = true;
             }
             else if (gameObject.GetComponent<AssembleModel>().snapped[2] || gameObject.GetComponent<AssembleModel>().snapped[3])
             {
                 gameObject.GetComponent<AssembleModel>().snapped[0] = true;
                 gameObject.GetComponent<AssembleModel>().snapped[1] = true;
+                oneWeightIn = true;
             }
         }
     }
