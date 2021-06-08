4eb9be440aa74cdd468609b060bb542705f758c9
Nghi Tran <pnghi320@gmail.com>
2021-06-07 22:09:39 -0700

    Show value of slider

---
+++ Assets/Scripts/SliderValueToText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SliderValueToText : MonoBehaviour
+{
+    public TextMeshProUGUI value;
+    public Slider sliderUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = GetComponent<TextMeshProUGUI>();
+        SetText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetText(){
+        value.SetText(sliderUI.value + " ");
+    }
+}
